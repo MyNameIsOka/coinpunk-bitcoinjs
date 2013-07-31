ae9dbcd987146c0d683df8f50d3ed75d6d7b4ff0
Kyle Drake <kyledrake@gmail.com>
2013-07-31 12:59:43 -0400

    separate charenc from crypto to prevent requirejs convert error

--- src/crypto-js/crypto.js
+++ src/crypto-js/crypto.js
@@ -5,60 +5,24 @@
  * http://code.google.com/p/crypto-js/wiki/License
  */
 
-// Global Crypto object
-var Crypto = module.exports = {};
+var charenc = require('./charenc');
 
 // Crypto utilities
-var util = Crypto.util = {
-
+module.exports.util = {
 	// Generate an array of any length of random bytes
 	randomBytes: function (n) {
 		for (var bytes = []; n > 0; n--)
 			bytes.push(Math.floor(Math.random() * 256));
 		return bytes;
-	},
-
+	}
 };
 
 // Crypto mode namespace
-Crypto.mode = {};
+module.exports.mode = {};
 
-// Crypto character encodings
-var charenc = Crypto.charenc = {};
+module.exports.UTF8 = charenc.UTF8;
+module.exports.Binary = charenc.Binary;
 
-// UTF-8 encoding
-var UTF8 = charenc.UTF8 = {
-
-	// Convert a string to a byte array
-	stringToBytes: function (str) {
-		return Binary.stringToBytes(unescape(encodeURIComponent(str)));
-	},
-
-	// Convert a byte array to a string
-	bytesToString: function (bytes) {
-		return decodeURIComponent(escape(Binary.bytesToString(bytes)));
-	}
-
-};
-
-// Binary encoding
-var Binary = charenc.Binary = {
-
-	// Convert a string to a byte array
-	stringToBytes: function (str) {
-		for (var bytes = [], i = 0; i < str.length; i++)
-			bytes.push(str.charCodeAt(i));
-		return bytes;
-	},
-
-	// Convert a byte array to a string
-	bytesToString: function (bytes) {
-		for (var str = [], i = 0; i < bytes.length; i++)
-			str.push(String.fromCharCode(bytes[i]));
-		return str.join("");
-	}
-
-};
-
+module.exports.charenc = charenc;
 module.exports.SHA256 = require('./sha256');
 module.exports.RIPEMD160 = require('./ripemd160');
